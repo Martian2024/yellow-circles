1af1c2abeef108faa834cb949a5284d93d7ef7e7
Arseniy <arseniy.kolomeytsev@gmal.com>
2021-12-23 14:11:40 +0300

    commit

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 import sys
 import random
 
-from PyQt5 import uic
+from PyQt5 import QtWidgets, QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setObjectName("MainWindow")
+        self.resize(497, 425)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(180, 320, 111, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 497, 21))
+        self.menubar.setObjectName("menubar")
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+        QtCore.QMetaObject.connectSlotsByName(self)
+        _translate = QtCore.QCoreApplication.translate
+        self.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Нарисовать круг"))
         self.pushButton.clicked.connect(self.draw)
         self.flag = False
 
@@ -16,7 +34,7 @@ class MyWidget(QMainWindow):
         if self.flag:
             qp = QPainter()
             qp.begin(self)
-            qp.setBrush(QColor(250, 250, 210))
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             r = random.randint(0, 100)
             qp.drawEllipse(random.randint(r, 497 - r), random.randint(r, 495 - r), r, r)
             qp.end()
